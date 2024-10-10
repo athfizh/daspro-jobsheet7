dd74a27a5708f5f54b1d893fe16760ddc2e00312
athfizh <athaullahafizh@gmail.com>
2024-10-10 14:34:56 +0700

    pertanyaan percobaan2

--- TugasBioskop.java
+++ TugasBioskop.java
@@ -3,29 +3,29 @@ public class TugasBioskop {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int jmlTiket;
+        int jmlTiket, i = 1;
         double diskon, hargaTiket = 50000, totalHarga;
 
         do {
-            System.out.print("Masukkan jumlah tiket terjual: ");
+            System.out.print("Jumlah tiket pelanggan " + i++ + ": ");
             jmlTiket = sc.nextInt();
-            if (jmlTiket < 0) 
+            if (jmlTiket > 0) 
             {
-                System.out.println("Jumlah tiket tidak valid. Silahkan input ulang!");
             }
             if (jmlTiket > 4) 
             {
-            diskon = 0.1;
+                diskon = 0.1;
             } else if (jmlTiket > 10) {
-            diskon = 0.15;
+                diskon = 0.15;
             } else {
-            diskon = 0;
-        }
-       
-    } while (jmlTiket < 0);
-    totalHarga = jmlTiket * hargaTiket;
-    System.out.println("Total tiket: " + jmlTiket);
-    System.out.println("Total harga: Rp " + totalHarga);
-    System.out.println("Total harga setelah diskon: Rp " + (totalHarga - (totalHarga * diskon)));
+                diskon = 0;
+            }
+            totalHarga = jmlTiket * hargaTiket;
+            System.out.println("Total tiket: " + jmlTiket);
+            System.out.println("Total harga: Rp " + totalHarga);
+            System.out.println("Total harga setelah diskon: Rp " + (totalHarga - (totalHarga * diskon)));
+            
+    } while (jmlTiket > 0);
+    System.out.println("Jumlah tiket tidak valid. Silahkan input ulang!");
 }
 }
