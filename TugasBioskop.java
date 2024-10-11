84d14ae73c2e2eccaf2ea3f85e0225cd8c3a9c26
athfizh <athaullahafizh@gmail.com>
2024-10-11 08:53:01 +0700

    tugas

--- TugasBioskop.java
+++ TugasBioskop.java
@@ -3,29 +3,46 @@ public class TugasBioskop {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int jmlTiket, i = 1;
-        double diskon, hargaTiket = 50000, totalHarga;
-
-        do {
-            System.out.print("Jumlah tiket pelanggan " + i++ + ": ");
-            jmlTiket = sc.nextInt();
-            if (jmlTiket > 0) 
-            {
+        int harga = 50000, jumlahTiket, totalHarga, i = 1, pendapatanHarian = 0, totalTiket = 0; 
+        double hargaDiskon = 0; 
+ 
+        System.out.println("-------------------PENGHITUNGAN TIKET BIOSKOP-------------------"); 
+        System.out.println("*Ketik 0 untuk keluar dari program"); 
+        System.out.println("----------------------------------------------------------------"); 
+        
+        do { 
+            System.out.print("Masukkan jumlah tiket pelanggan ke " + i + " : "); 
+            jumlahTiket = sc.nextInt(); 
+ 
+            if (jumlahTiket < 0) { 
+                System.out.println("Nilai tidak valid. Masukkan ulang jumlah tiket"); 
+                continue; 
+            } else if (jumlahTiket == 0) { 
+                continue; 
             }
-            if (jmlTiket > 4) 
-            {
-                diskon = 0.1;
-            } else if (jmlTiket > 10) {
-                diskon = 0.15;
-            } else {
-                diskon = 0;
-            }
-            totalHarga = jmlTiket * hargaTiket;
-            System.out.println("Total tiket: " + jmlTiket);
-            System.out.println("Total harga: Rp " + totalHarga);
-            System.out.println("Total harga setelah diskon: Rp " + (totalHarga - (totalHarga * diskon)));
+            totalHarga = jumlahTiket * harga; 
+            System.out.println("Total harga : Rp " + totalHarga); 
+ 
+            if (jumlahTiket > 4 && jumlahTiket <= 10) { 
+                hargaDiskon = totalHarga - (totalHarga * 0.1); 
+                System.out.println("Diskon 10% menjadi : Rp " + hargaDiskon); 
+            } else if (jumlahTiket > 10) { 
+                hargaDiskon = totalHarga - (0.15 * totalHarga); 
+                System.out.println("Diskon 15% menjadi : Rp " + hargaDiskon); 
+            } else { 
+                hargaDiskon = totalHarga; 
+            } 
+ 
+            pendapatanHarian += hargaDiskon; 
+            totalTiket += jumlahTiket;
             
-    } while (jmlTiket > 0);
-    System.out.println("Jumlah tiket tidak valid. Silahkan input ulang!");
-}
-}
+            i++; 
+            
+        } while (jumlahTiket != 0); 
+        
+        System.out.println("----------------------------------------------------------------");
+        System.out.println("Total tiket terjual sehari : " + totalTiket);
+        System.out.println("Total harga penjualan tiket sehari : Rp " + pendapatanHarian); 
+        System.out.println("----------------------------------------------------------------");      
+    } 
+} 
